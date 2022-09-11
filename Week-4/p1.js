7b6453ff23b4175964595bd5d4a56cc51842f2f1
Durgesh Kumar Singh <dk829445@gmail.com>
2022-09-11 20:05:45 +0530

    feat: solved custom promise problem

---
+++ Week-4/p1.js
@@ -0,0 +1,92 @@
+//function for getting a random number
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 500);
+};
+
+//function to check if given number is divisible by 5 or not
+const checkIsDivisible = (num) => {
+  if (num % 5 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const promiseStates = {
+    pending : 0, fulfilled : 1, rejected : 2, 
+}
+
+function customPromise(executor) {
+  const { pending, fulfilled, rejected } = promiseStates;
+  let state = pending;
+  let value = null;
+  let executionTime = null;
+
+  function resolve(result) {
+    if (state !== pending) return;
+    console.time();
+    state = fulfilled;
+    value = result;
+    executionTime = console.timeEnd();
+  }
+  function reject(err) {
+    if (state !== pending) return;
+    console.time();
+    state = rejected;
+    value = err;
+    executionTime = console.timeEnd();
+  }
+  this.then = function (success) {
+    if (state === fulfilled) {
+      success(value);
+    }
+    return this;
+  };
+  this.catch = function (failure) {
+    if (state === rejected) {
+      failure(value);
+    }
+    return this;
+  };
+  executor(resolve, reject);
+}
+
+let promise1 = new customPromise((resolve, reject) => {
+  const randomNumber = getRandomNumber();
+  if (checkIsDivisible(randomNumber)) {
+    reject(
+      `The promise is rejected because the number ${randomNumber} is divisible by 5`
+    );
+  } else {
+    resolve(
+      `The promise is resolved because the number ${randomNumber} is not divisible by 5`,
+    );
+  }
+});
+let promise2 = new customPromise((resolve, reject) => {
+  let randomNumber = getRandomNumber();
+  if (checkIsDivisible(randomNumber)) {
+    reject(
+      `The promise is rejected because the number ${randomNumber} is divisible by 5`
+    );
+  } else {
+    resolve(
+      `The promise is resolved because the number ${randomNumber} is not divisible by 5`
+    );
+  }
+});
+
+promise1
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((data) => {
+    console.error(data);
+  });
+promise2
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((data) => {
+    console.error(data);
+  });
