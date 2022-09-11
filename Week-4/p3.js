c552b7bb23c1f11d5a20c017e01d3b4f62e2c9ed
Durgesh Kumar Singh <dk829445@gmail.com>
2022-09-11 13:03:39 +0530

    feat: solved fibonacci question

---
+++ Week-4/p3.js
@@ -0,0 +1,35 @@
+const getFibonacci = (num) => {
+  let i = 0;
+  let currentTarget = 0;
+  let nextTarget = i + 1;
+  const obj = {
+    [Symbol.iterator]() {
+      return this;
+    },
+    next: () => {
+      if (i === 0) {
+        i++;
+        return { value: 0, done: i > num };
+      }
+      if (i === 1) {
+        i++;
+        return { value: 1, done: i > num };
+      }
+      if (i >= 2) {
+        i++;
+        let sum = currentTarget + nextTarget;
+        currentTarget = nextTarget;
+        nextTarget = sum;
+        return { value: sum, done: i > num };
+      }
+    },
+  };
+  return obj;
+};
+
+for (const element of getFibonacci(13)) {
+  if (element === 0) {
+    console.log("The Fibonacci Series is :");
+  }
+  console.log(element);
+}
