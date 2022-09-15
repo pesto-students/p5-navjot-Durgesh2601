bf665efb40c018020064f025468e7bdc7d34b9c1
Durgesh Kumar Singh <dk829445@gmail.com>
2022-09-16 00:38:16 +0530

    feat: test cases for calculator question

---
+++ Week-5/p4.js
@@ -0,0 +1,12 @@
+const mathOperations = {
+  add: function (a, b) {
+    return a + b;
+  },
+  substract: function (a, b) {
+    return a - b;
+  },
+  product: function (a, b) {
+    return a * b;
+  },
+};
+module.exports = mathOperations;
