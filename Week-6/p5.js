f2f1f25b59095b589cf848c7b470c163d30c1fda
Durgesh Kumar Singh <dk829445@gmail.com>
2022-09-27 11:21:14 +0530

    second approach

--- Week-6/p5.js
+++ Week-6/p5.js
@@ -1,5 +1,6 @@
 const arr = [5, 10, 3, 2, 50, 80];
-const b = 79;
+const b = 45;
+//approach 1st
 const checkDiff = (arr, num) => {
     for(let i = 0; i < arr.length; i++){
         for(let j = i+1; j < arr.length; j++) {
@@ -10,5 +11,15 @@ const checkDiff = (arr, num) => {
     }
     return 0;
 }
+//2nd approach
+const checkDiff2 = (arr, num) => {
+    for(let i = 0; i < arr.length; i++) {
+        if(arr.includes(b + arr[i])) {
+            return 1;
+        }
+    }
+    return 0;
+}
 
 console.log(checkDiff(arr, b));
+console.log(checkDiff2(arr, b));
