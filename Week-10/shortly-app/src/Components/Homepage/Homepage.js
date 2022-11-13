2f25ab8f004c5ee7bf476074f017d4ed19455a95
Durgesh2607 <105216675+Durgesh2607@users.noreply.github.com>
2022-11-13 22:49:39 +0530

    home page component

---
+++ Week-10/shortly-app/src/Components/Homepage/Homepage.js
@@ -0,0 +1,8 @@
+import "./Homepage.css";
+export const Homepage = () => {
+    return <>
+    <section classname="homepage-container">
+    <h1>Hello</h1>
+    </section>
+    </>
+}
--- Week-10/shortly-app/src/Components/index.js
+++ Week-10/shortly-app/src/Components/index.js
@@ -1,10 +1,12 @@
 import { Footer } from "./Footer/Footer"
+import { Homepage } from "./Homepage/Homepage"
 import { Navbar } from "./Navbar/Navbar"
 
 
 export const Component = () => {
     return <>
     <Navbar/>
+    <Homepage/>
     <Footer/>
     </>
 }
