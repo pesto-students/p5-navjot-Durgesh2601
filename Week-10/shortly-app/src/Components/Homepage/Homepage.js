2f01a883244951dcf608c4ec8385534d01594695
Durgesh2607 <105216675+Durgesh2607@users.noreply.github.com>
2022-11-14 01:33:30 +0530

    integrated api

--- Week-10/shortly-app/src/Components/Homepage/Homepage.js
+++ Week-10/shortly-app/src/Components/Homepage/Homepage.js
@@ -1,7 +1,22 @@
 import "./Homepage.css";
 import img from "../../assets/imageStatic.png";
 import { UrlShortner } from "./UrlShortner";
+import { UrlLists } from "./UrlLists";
+import { useState } from "react";
+const defaultData = [
+  {
+    code: "KCveN",
+    short_link: "shrtco.de/KCveN",
+    full_short_link: "https://shrtco.de/KCveN",
+    short_link2: "9qr.de/KCveN",
+    full_short_link2: "https://9qr.de/KCveN",
+    share_link: "shrtco.de/share/KCveN",
+    full_share_link: "https://shrtco.de/share/KCveN",
+    original_link: "http://example.org/very/long/link.html",
+  },
+];
 export const Homepage = () => {
+  const [data, setData] = useState(defaultData);
   return (
     <div className="main-conatiner">
       <section className="sub-container">
@@ -16,7 +31,8 @@ export const Homepage = () => {
           <img src={img} alt="img" height={300} width={400} />
         </div>
       </section>
-      <UrlShortner />
+      <UrlShortner data={data} setData={setData} />
+      <UrlLists data={data} setData={setData} />
     </div>
   );
 };
--- Week-10/shortly-app/src/Components/Homepage/UrlShortner.js
+++ Week-10/shortly-app/src/Components/Homepage/UrlShortner.js
@@ -1,10 +1,33 @@
 import { useState } from "react";
-export const UrlShortner = () => {
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
+import Button from "react-bootstrap-button-loader";
+import "react-toastify/dist/ReactToastify.css";
+export const UrlShortner = ({ data, setData }) => {
   const [urlValue, setUrlValue] = useState("");
+  const [loading, setLoading] = useState(false);
   const handleUrlShortner = () => {
     if (urlValue === "") {
-      return window.alert(`Url can't be empty!`);
+      return toast(`Url can't be empty!`);
     }
+    setLoading(true);
+    axios
+      .post(`https://api.shrtco.de/v2/shorten?url=${urlValue}`)
+      .then((res) => {
+        if (res?.status === 201) {
+          setLoading(false);
+          toast(`Url shortened successfully!`);
+          const result = res?.data?.result;
+          setData([result, ...data]);
+          setUrlValue("");
+        }
+      })
+      .catch((err) => {
+        setLoading(false);
+        toast(err?.message);
+        setUrlValue("");
+        console.error(err);
+      });
   };
   return (
     <>
@@ -12,11 +35,17 @@ export const UrlShortner = () => {
         <input
           className="url-input"
           placeholder="Shorten a link here..."
+          value={urlValue}
           onChange={(e) => setUrlValue(e?.target?.value)}
         />
-        <button className="btn-shortner" onClick={handleUrlShortner}>
+        <Button
+          loading={loading}
+          className="btn-shortner"
+          onClick={handleUrlShortner}
+        >
           Shorten it!
-        </button>
+        </Button>
+        <ToastContainer />
       </div>
     </>
   );
