1c6683dfe0dd46b57f77c182d71aed066e3e85f3
Durgesh2607 <105216675+Durgesh2607@users.noreply.github.com>
2022-12-13 23:17:40 +0530

    created app setup

---
+++ Week-11/Light-switch/src/redux/reducer.js
@@ -0,0 +1,24 @@
+import { createStore } from "redux";
+import { TOGGLE } from "./actions";
+
+const initialState = {
+  background: "black",
+  textContent: "dark",
+};
+
+const reducer = (state = initialState, action) => {
+  const { background, textContent } = state;
+  switch (action.type) {
+    case TOGGLE: {
+      return {
+        background: background === "black" ? "white" : "black",
+        textContent: textContent === "dark" ? "lit" : "dark",
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
+
+export const store = createStore(reducer);
--- Week-11/light-switch/src/LightSwitch/index.js
+++ Week-11/light-switch/src/LightSwitch/index.js
@@ -1,17 +1,25 @@
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 
 export const LightSwitch = () => {
     const [isOn, setIsOn] = useState(false);
+    const state = useSelector((state) => state);
+    const { background, textContent } = state;
+    const dispatch = useDispatch();
+    console.log(background, textContent)
+    const handleFlip = () => {
+        dispatch({type:'TOGGLE'});
+    }
     return <>
     <div>
         <h3>
-        The room is {isOn ? 'lit' : 'dark'}
+        The room is {textContent}
         </h3>
-        <button onClick={() => setIsOn(!isOn)}> 
+        <button onClick={handleFlip}> 
             flip
         </button>
     </div>
-    <div className={`room-${isOn ? 'lit' : 'dark'}`}>
+    <div className={`room-${textContent}`}>
     </div>
     </>
 }
