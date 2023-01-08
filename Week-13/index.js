f35e04378e5b417e85fff942c3025b7d84a7697f
Durgesh2607 <dk829445@gmail.com>
2023-01-08 22:53:39 +0530

    basic setup

--- Week-13/index.js
+++ Week-13/index.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const app = express();
 
+const http = require('http');
+
 require('dotenv').config();
 
 const api_key = `${process.env.API_KEY}`;
 
-app.get('/', (req, res) => {
-    res.send(`Hello: ${api_key}`);
+app.get('/weather', (req, res) => {
+    console.log('hello')
+    // res.send(`Hello: ${api_key}`);
 })
 
-app.listen((8080, () => {
-    console.log(`Server listening on port 8080`);
-}));
+const options = {
+    hostname: 'jsonplaceholder.typicode.com',
+    port: 80,
+    path: '/todos/1',
+}
+http.get(options, (res) => {
+    console.log(res.statusCode);
+})
+// app.listen((5000, () => {
+//     console.log(`Server listening on port 5000`);
+// }));
+
